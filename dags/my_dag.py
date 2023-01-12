cf2a5e70b972fe6cb7ab8472eea1bb86b058fdba
alexis sair <alexis.sair@xpln.ai>
2023-01-12 11:39:59 +0100

    first dag

--- dags/my_dag.py
+++ dags/my_dag.py
@@ -1,25 +1,3 @@
-#
-# Licensed to the Apache Software Foundation (ASF) under one
-# or more contributor license agreements.  See the NOTICE file
-# distributed with this work for additional information
-# regarding copyright ownership.  The ASF licenses this file
-# to you under the Apache License, Version 2.0 (the
-# "License"); you may not use this file except in compliance
-# with the License.  You may obtain a copy of the License at
-#
-#   http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing,
-# software distributed under the License is distributed on an
-# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-# KIND, either express or implied.  See the License for the
-# specific language governing permissions and limitations
-# under the License.
-"""
-### Tutorial Documentation
-iDocumentation that goes along with the Airflow tutorial located
-[here](https://airflow.apache.org/tutorial.html)
-"""
 from __future__ import annotations
 
 # [START tutorial]
@@ -36,9 +14,9 @@ from airflow.operators.bash import BashOperator
 # [END import_module]
 
 
-# [START instantiate_dag]
+# @Alexis: is the code pushed ?
 with DAG(
-    "tutorial",
+    "Alexis-dag",
     description="A simple tutorial DAG",
     schedule=timedelta(days=1),
     start_date=datetime(2021, 1, 1),
